7024ea62e4326dc9bf0dd5d6ac6cae74e71cf4c5
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2023-08-14 20:16:32 +0200

    addressed suggestions from review

--- assets/js/people.js
+++ assets/js/people.js
@@ -1,12 +1,10 @@
 var peopleController = (function(jQuery) {
   const CLASS_ACTIVE = 'active';
   const CLASS_HIDDEN = 'd-none';
-
-  var $cardBody = jQuery('.card');
-  var $cardBodyItem = jQuery('.contributors .card-body');
+  const CARD_BODY = jQuery('.card');
 
   var showColaboratorDetails = function(index) {
-    var item = jQuery('#colaborator-'+index);
+    let item = jQuery('#colaborator-'+index);
 
     if (item.hasClass(CLASS_HIDDEN)) {
       item.addClass(CLASS_ACTIVE);
@@ -18,11 +16,11 @@ var peopleController = (function(jQuery) {
   }
 
   var initEvents = function() {
-    $cardBody.on('mouseover', function() {
+    CARD_BODY.on('mouseover', function() {
       showColaboratorDetails(jQuery(this).data('index'));
     });
 
-    $cardBody.on('mouseout', function() {
+    CARD_BODY.on('mouseout', function() {
       showColaboratorDetails(jQuery(this).data('index'));
     });
   };
